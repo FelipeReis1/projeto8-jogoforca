120b257064aabc9ea16f686fa8a84e319b1c6ecc
FelipeReis1 <feliperg15@yahoo.com.br>
2022-11-24 20:10:57 -0300

    inicializando repositorio - layout inicial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="containerPrincipal">
+      <img className="forca" src="./assets/forca0.png" alt=""></img>
+      <button className="escolherPalavra">
+        <p>Escolher Palavra</p>
+      </button>
+      <div className="placeHolderLetras">
+        <p>_ _ _ _ _ _ _ _</p>
+      </div>
+      <div className="teclado">
+        <button className="botaoLetra">
+          <p>A</p>
+        </button>
+      </div>
+      <div className="resposta">
+        <p>Já sei a palavra!</p>
+        <input></input>
+        <button className="chutar">
+          <p>Chutar</p>
+        </button>
+      </div>
     </div>
   );
 }
-
-export default App;
